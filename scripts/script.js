37ebe23a78385f82297791ccd03fa9b893d50b99
Leandro <leoroan@gmail.com>
2023-06-10 15:05:30 -0300

    agregando cositas para el tp

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-console.log("hello cuentitas!");
+console.log("Hello cuentitas!");
+
+
+/// START OF THE HISTORY 
+let nameUser = " ";
+let saludo = (nom) => { return `Hola ${nom}, Bienvenido a cuentitas!` }
+
+if (nameUser == " " || nameUser == null) {
+  nameUser = prompt("Hola 🙎‍♂️ o 🙎, comencemos por saber quién sos!, me dirías tu nombre?");
+  while (nameUser == null || nameUser == " " || nameUser == "") {
+    nameUser = prompt("Te pedí un nombre!!");
+  }
+}
+
+// SI HAY USUARIO
+if (nameUser !== " ") {
+  prompt(saludo(nameUser));
+  console.log("Lets start!");
+}
+
