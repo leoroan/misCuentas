a8e23e13eefb8efa0a092bdef635e9b231bc6081
Leandro Maselli <lmaselli@transporte.gba.gob.ar>
2023-07-12 14:00:45 -0300

    implementando eventListeners

--- scripts/scriptV2.js
+++ scripts/scriptV2.js
@@ -1,11 +1,36 @@
-// Obtén una referencia al botón por su ID
+
+// Obtener el cuerpo principal (box)
+const mainBox = document.getElementById("mainBox");
+
+// Obtener el cuerpo principal (box)
+const cardResultDisplay = document.getElementById("cardResultDisplay");
+
+
+// Obtener el botón por su ID
 const button = document.getElementById("mainButton");
 
-// Agrega un evento de clic al botón
-button.addEventListener("click", function (e) {
+button.addEventListener('click', function (e) {
     e.preventDefault();
     const montoInput = document.getElementById("montoInput").value;
     const plazoInput = document.getElementById("plazoInput").value;
-    console.log(montoInput, plazoInput);
-});
+    let cardBienvenida = document.getElementById("cardBienvenida");
+    let cardInicial = document.getElementById("cardInicial");
 
+    document.getElementById('montoInput').addEventListener('input', function() { condicion ? hacerAlgo() : hacerOtraCosa(); });
+
+    function hacerAlgo() {
+        console.log("haciendo algo");
+    }
+    function hacerOtraCosa() {
+        console.log("haciendo OtraCosa");
+    }
+
+    if ((plazoInput > 0 && plazoInput <= 36) && montoInput > 0)  {
+        cardBienvenida.classList.add('hide');
+        cardInicial.classList.add('hide');
+        cardResultDisplay.style.display = 'block';
+    } else {
+        document.getElementById("validationPlazoInputFeedback").style.display = 'block';
+        document.getElementById("validationMontoInputFeedback").style.display = 'block';
+    }
+});
